6478fdcf02468766234ae466b05816bab7436ff5
HarishBhatta <bhattaharish10@gmail.com>
2024-03-01 15:53:37 +0545

    Search part edited

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,6 +5,9 @@ import ImgIcon from '../../public/search.svg'
 
 const Search = () => {
   const { isOpen, handleClose } = useContext(SearchContext);
+  function handleSearch(term){
+    console.log(term);
+  }
   return (
     <div className={` ${isOpen ? 'top-0' : '-top-full'} w-screen left-0 right-0 absolute h-1/2 shadow-2xl transition-all duration-300 z-20 bg-white`}>
 
@@ -14,7 +17,7 @@ const Search = () => {
         </svg>
       </div>
       <p className="text-lg text-slate-500 font-medium ml-12">What are you looking for?</p>
-      <div className="ml-12 w-[80%] mt-5 select-none flex  border-b-slate-300 border-b-[3px] gap-10"><input className=' outline-none placeholder:text-slate-500 font-bold text-2xl  pl-1 pb-0 search w-[93%]' type="search" id="gsearch" name="gsearch" placeholder='Search Products'></input>
+      <div className="ml-12 w-[80%] mt-5 select-none flex  border-b-slate-300 border-b-[3px] gap-10"><input className=' outline-none placeholder:text-slate-500 font-bold text-2xl  pl-1 pb-0 search w-[93%]' type="search" onChange={(e)=> {handleSearch(e.target.value)}} id="gsearch" name="gsearch" placeholder='Search Products'></input>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className='fill-slate-600 stroke-2 mb-1' class="bi bi-search" viewBox="0 0 16 16">
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
         </svg>
